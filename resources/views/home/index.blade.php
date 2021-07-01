0255502067e2026574f7681777f6beb371cd75b9
Mladshev Evgeniy <fixter.spb@gmail.com>
2021-07-01 15:12:49 +0300

    Deleted old links in index templates

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,9 +1,3 @@
 <x-app-layout>
-    <h1>Главная страничка</h1>
-    <ul class="block">
-        <li><a href="/">Главная страничка</a></li>
-        <li><a href="/doctors">Доктора</a></li>
-        <li><a href="/schedule">Расписание</a></li>
-    </ul>
     <x-doctors.hello></x-doctors.hello>
 </x-app-layout>
