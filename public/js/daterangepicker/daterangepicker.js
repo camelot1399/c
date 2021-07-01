04cdf9a46e6db834ae553d4e8d62354d21bab7f6
Mladshev Evgeniy <fixter.spb@gmail.com>
2021-07-01 18:38:00 +0300

    gitignore

--- public/js/daterangepicker/daterangepicker.js
+++
@@ -1,9 +0,0 @@
-/*booking-box*/
-function dataRangePickerOpen(){
-    if(document.getElementById('daterangepicker_box').style.display==='none'){
-        document.getElementById('daterangepicker_box').setAttribute("style", "display: block; top: 344px; left: 1150px; right: auto;");
-    }else{
-        document.getElementById('daterangepicker_box').style.display='none';
-    }
-}
-daterangepicker.addEventListener("click", dataRangePickerOpen);
