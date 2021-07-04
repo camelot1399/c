3967b818f84aba3144e3404ec4ef0757942db831
gwinbleind <sunubucocA1>
2021-07-04 18:59:27 +0300

    Переместил массив из Doctors.php в SpecialistSeeder.php

--- database/seeders/SpecialistSeeder.php
+++ database/seeders/SpecialistSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Doctors;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
@@ -15,8 +14,81 @@ class SpecialistSeeder extends Seeder
      */
     public function run()
     {
-        $doctorsObj = new Doctors();
-        $doctors = $doctorsObj->doctors;
+        $doctors = [
+            [
+                'name' => 'Диедра Спанглер',
+                'speciality' => 'MDS - Пародонтология и оральная имплантология, BDS',
+                'rating' => 1,
+                'location' => 'Флорида, США',
+                'photo' => 'doctor-01.jpg',
+                'coast' => 500,
+                'status' => 1
+            ],
+            [
+                'name' => 'Николай Чавес',
+                'speciality' => 'BDS, MDS - Челюстно-лицевая хирургия',
+                'rating' => 4,
+                'location' => 'Нью-Йорк, США',
+                'photo' => 'doctor-02.jpg',
+                'coast' => 500,
+                'status' => 1
+            ],
+            [
+                'name' => 'Бедилия Эллиот',
+                'speciality' => 'MBBS, MD - Лечебное дело, DNB - Кардиология',
+                'rating' => 2,
+                'location' => 'Джорджия, США',
+                'photo' => 'doctor-03.jpg',
+                'coast' => 500,
+                'status' => 0
+            ],
+            [
+                'name' => 'Александра Фостер',
+                'speciality' => 'MBBS, MS - Общая хирургия, MCh - Урология',
+                'rating' => 1,
+                'location' => 'Нью-Йорк, США',
+                'photo' => 'doctor-04.jpg',
+                'coast' => 500,
+                'status' => 1
+            ],
+            [
+                'name' => 'Диедра Спанглер',
+                'speciality' => 'MDS - Пародонтология и оральная имплантология, BDS',
+                'rating' => 5,
+                'location' => 'Флорида, США',
+                'photo' => 'doctor-01.jpg',
+                'coast' => 500,
+                'status' => 1
+            ],
+            [
+                'name' => 'Николай Чавес',
+                'speciality' => 'BDS, MDS - Челюстно-лицевая хирургия',
+                'rating' => 5,
+                'location' => 'Нью-Йорк, США',
+                'photo' => 'doctor-02.jpg',
+                'coast' => 500,
+                'status' => 1
+            ],
+            [
+                'name' => 'Бедилия Эллиот',
+                'speciality' => 'MBBS, MD - Лечебное дело, DNB - Кардиология',
+                'rating' => 3,
+                'location' => 'Джорджия, США',
+                'photo' => 'doctor-03.jpg',
+                'coast' => 500,
+                'status' => 1
+            ],
+            [
+                'name' => 'Александра Фостер',
+                'speciality' => 'MBBS, MS - Общая хирургия, MCh - Урология',
+                'rating' => 2,
+                'location' => 'Нью-Йорк, США',
+                'photo' => 'doctor-04.jpg',
+                'coast' => 500,
+                'status' => 1
+
+            ],
+        ];
         foreach ($doctors as $doctor) {
             \DB::table('specialists')->insert([
                 'user_id' => 1,
