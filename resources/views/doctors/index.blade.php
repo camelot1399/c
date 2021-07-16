d5a0f1de97c0acf420dc450574e3a5e84e6a9b43
camelot1399 <camelot1399@mail.ru>
2021-07-16 14:44:50 +0300

    ds32 refactored cardCarousel

--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -1,14 +1,13 @@
 <x-app-layout>
     <div class="bg-div-gray-200 p-80px">
-        <card-carousel-component
+        <Specialists-List
             :doctors="{{ json_encode($doctors) }}"
-            :slider="false"
             routedoctor="{{ route('doctors.show', ["specialist" => "xxx"]) }}"
             routeshedule="{{ route('schedule.index', ["specialist" => "xxx"]) }}"
         />
     </div>
 
     @push('scripts')
-        <script src="{{ mix('js/cardCarousel.js') }}" defer></script>
+        <script src="{{ mix('js/specialistsList.js') }}" defer></script>
     @endpush
 </x-app-layout>
